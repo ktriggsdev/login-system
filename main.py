a0ebf44b9077f3e2d3b9926d5dda05b730da6ba8
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-04-22 13:14:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,77 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+    
+if authentication_status:
+    try:
+        if authenticator.reset_password(username, 'Reset password'):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
+        
+try:
+    if authenticator.register_user('Register user', preauthorization=False):
+        st.success('User registered successfully')
+except Exception as e:
+    st.error(e)
+    
+
+    
+try:
+    username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
+    if username_forgot_pw:
+        st.success('New password sent securely')
+        # Random password to be transferred to user securely
+    else:
+        st.error('Username not found')
+except Exception as e:
+    st.error(e)
+    
+try:
+    username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
+    if username_forgot_username:
+        st.success('Username sent securely')
+        # Username to be transferred to user securely
+    else:
+        st.error('Email not found')
+except Exception as e:
+    st.error(e)
+    
+if authentication_status:
+    try:
+        if authenticator.update_user_details(username, 'Update user details'):
+            st.success('Entries updated successfully')
+    except Exception as e:
+        st.error(e)
+        
+    
+with open('config.yaml', 'w') as file:
+      yaml.dump(config, file, default_flow_style=False)
+
+    
+    
